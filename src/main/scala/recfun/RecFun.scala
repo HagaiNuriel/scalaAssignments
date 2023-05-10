b2cd9af7df4c427ba4dfa4b06dcf55ba7bf83795
hagainuriel <hagain@salt.security>
2023-05-10 10:51:16 +0300

    recursion assignment

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
@@ -15,16 +17,47 @@ object RecFun extends RecFunInterface {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+//    if (c == 0 || c == r) 1
+//    else pascal(c-1, r-1) + pascal(c, r-1)
+    @tailrec
+    def pascalAux(currCol: Int, currRow: Int, prevTriangleRow: Array[Int], currTriangleRow: Array[Int]): Int = {
+      currTriangleRow(currCol) = (if (currCol > 0) prevTriangleRow(currCol - 1) else 0) + (if (currCol < currRow) prevTriangleRow(currCol) else 0)
 
+      if ((currCol == c) && (currRow == r)) currTriangleRow(currCol)
+      else if (currCol < currRow) pascalAux(currCol + 1, currRow, prevTriangleRow, currTriangleRow)
+      else pascalAux(0, currRow + 1, currTriangleRow, new Array(_length = currRow + 2))
+    }
+
+    if ((c == 0) && (r == 0)) 1
+    else pascalAux(0, 1, Array(1), Array(0, 0))
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceRec(chars: List[Char], openParentheses: Int): Boolean = {
+      if (openParentheses < 0) false
+      else if (chars.isEmpty) openParentheses == 0
+      else {
+        val c = chars.head
+        val currOpenParentheses =
+          if (c == '(') openParentheses + 1
+          else if (c == ')') openParentheses - 1
+          else openParentheses
+        balanceRec(chars.tail, currOpenParentheses)
+      }
+    }
+
+    balanceRec(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
